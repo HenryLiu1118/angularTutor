0f236723a072b9d2bc4e627bb1e1583cbd06ffa1
henry <henry@henrys-MacBook-Air.local>
2019-05-16 05:44:06 -0700

    first version

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Student } from '../../shared/student.model';
+import { Food } from '../../shared/Food.model';
+import { Menu } from '../../shared/Food.model';
 
 @Component({
   // select: '[app-server]',
@@ -8,6 +9,60 @@ import { Student } from '../../shared/student.model';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+
+    foods: Food[] = [new Food('Beef', 'assets/images/beef.jpg'),
+      new Food('Chicken', 'assets/images/chicken.jpg'),
+      new Food('Egg', 'assets/images/egg.jpg'),
+      new Food('Peanut', 'assets/images/penaut.jpg'),
+      new Food('Potato', 'assets/images/potato.jpg'),
+      new Food('Tomato', 'assets/images/tomato.jpg'),
+      new Food('Pepper', 'assets/images/pepper.jpg'),
+      new Food('Dry Pepper', 'assets/images/dry_pepper.jpg')];
+
+    IngredientsName = ['ChickenPeanut', 'BeefPotato', 'BeefPepper', 'EggTomato',
+    'ChickenDry Pepper'];
+
+    menus: Menu[] = [new Menu('Kung Pao chicken', 'ChickenPeanut', 'assets/images/Kung Pao chicken.jpg'),
+      new Menu('Braised Beef with Potatoes', 'BeefPotato', 'assets/images/Braised Beef with Potatoes.jpg'),
+      new Menu('Black Pepper Beef', 'BeefPepper', 'assets/images/Black Pepper Beef.jpeg'),
+      new Menu('Scrambled Eggs with Tomato', 'EggTomato', 'assets/images/Scrambled Eggs with Tomato.jpg'),
+      new Menu('Spicy chicken', 'ChickenDry Pepper', 'assets/images/Spicy chicken.jpg')];
+
+    MealName = 'Your Meal: ';
+    MealUrl = 'assets/images/najianyujian.jpg';
+    Ingredients = [];
+
+    Chooseingredients(name) {
+      if (this.Ingredients.length > 1) {
+        return;
+      }
+      this.Ingredients.push(name);
+      this.Ingredients.sort();
+    }
+
+    OrderMenu() {
+      this.MealName = 'test';
+      if (this.Ingredients.length > 1) {
+          const idx = this.IngredientsName.indexOf(this.Ingredients.join(''));
+          if (idx === -1) {
+            this.MealName = 'Sorry 臣妾做不到';
+            this.MealUrl = 'assets/images/najianyujian.jpg';
+          } else {
+            this.MealName = this.menus[idx].name;
+            this.MealUrl = this.menus[idx].URL;
+          }
+      } else {
+        this.MealName = 'You need one more Ingredient';
+      }
+    }
+
+    ResetMeal() {
+      this.Ingredients = [];
+      this.MealName = 'Your Meal';
+      this.MealUrl = 'assets/images/najianyujian.jpg';
+    }
+
+  /*
     serverId = 10;
     serverStatus = 'offLine';
     counter = 0;
@@ -40,5 +95,5 @@ export class ServerComponent {
     // Event Binding
     onUpdateUserName(event: Event) {
       this.userName = (<HTMLInputElement>event.target).value;
-    }
+    }*/
 }
---
+++ src/shared/Food.model.ts
@@ -0,0 +1,12 @@
+export class Food {
+  constructor(public name: string,
+              public URL: string) {
+  }
+}
+
+export class Menu {
+  constructor(public name: string,
+              public ingredientsname: string,
+              public URL: string) {
+  }
+}
